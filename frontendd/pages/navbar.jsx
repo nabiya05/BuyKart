61b17c950aecdeab6b81872c590eb8f787642a5d
Nabiya Patan <patannabiya05@gmail.com>
2026-06-30 17:17:12 +0530

    Replace href links with Link components in navbar

--- frontendd/pages/navbar.jsx
+++ frontendd/pages/navbar.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { SearchContext } from "./context";
+import { Link } from "react-router-dom";
 import {
   Navbar,
   Container,
@@ -22,8 +23,9 @@ export default function MainNavbar() {
   return (
     <Navbar bg="white" expand="lg" className="shadow-sm">
       <Container>
-        <Navbar.Brand href="/products">
-          🛍 BuyKart
+        <Navbar.Brand>
+          <Nav.Link as={Link} to="/">BuyKart</Nav.Link>
+          
         </Navbar.Brand>
 
         <Navbar.Toggle />
@@ -48,20 +50,18 @@ export default function MainNavbar() {
           </Form>
 
           <Nav className="ms-auto">
-            <Nav.Link href="/cart">
-              🛒 Cart
-            </Nav.Link>
+            <Nav.Link as={Link} to="/cart">🛒Cart</Nav.Link>
 
             <DropdownButton
               title="Account"
               variant="outline-primary"
             >
-              <Dropdown.Item href="/user/signup">
-                Signup
+              <Dropdown.Item>
+              <Nav.Link as={Link} to="/user/signup">Signup</Nav.Link>
               </Dropdown.Item>
 
-              <Dropdown.Item href="/user/login">
-                Login
+              <Dropdown.Item>
+                <Nav.Link as={Link} to="/user/login">login</Nav.Link>
               </Dropdown.Item>
             </DropdownButton>
           </Nav>
